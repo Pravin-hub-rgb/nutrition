d58fdd777612f4667fbd10e4ca8a935b632b56ab
Pravin-hub-rgb <64892563+Pravin-hub-rgb@users.noreply.github.com>
2021-03-28 18:01:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//     document.getElementById("main").style.marginLeft = "250px";
+//   }
+  
+//   function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//     document.getElementById("main").style.marginLeft= "0";
+//   }
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  // document.querySelector(".sidenav").style.display="block";
+}
+
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";  
+  // document.querySelector(".sidenav").style.display="none";
+}
+
