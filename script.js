879fbc93539abb6be01c211327df2ef9c1c9e300
Pravin-hub-rgb <64892563+Pravin-hub-rgb@users.noreply.github.com>
2022-07-11 18:16:29 +0530

    change according to side menu

--- script.js
+++ script.js
@@ -8,14 +8,18 @@
 //     document.getElementById("main").style.marginLeft= "0";
 //   }
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  // document.querySelector(".sidenav").style.display="block";
-}
-
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";  
-  // document.querySelector(".sidenav").style.display="none";
-}
-
+$('.btn').click(function() {
+  $(this).toggleClass("click");
+  $('.sidebar').toggleClass("show");
+});
+$('.feat-btn').click(function() {
+  $('nav ul .feat-show').toggleClass("show");
+  $('nav ul .first').toggleClass("rotate");
+});
+$('.serv-btn').click(function() {
+  $('nav ul .serv-show').toggleClass("show1");
+  $('nav ul .second').toggleClass("rotate");
+});
+$('nav ul li').click(function() {
+  $(this).addClass("active").siblings().removeClass("active");
+});
